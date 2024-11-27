c0aa1ca37180df6ff78497e630c45bd72014b13b
Krunal Amin <kishanamin97@gmail.com>
2024-11-26 22:26:08 -0600

    pwa support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,6 +34,19 @@ const Disclaimer: DisclaimerComponent = ({ Text, Link }) => (
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <head>
+        <meta name="application-name" content="Your App Name" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Your App Name" />
+        <meta name="description" content="Your app description" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
       <Script
         src="/static/charting_library/charting_library.standalone.js"
         strategy="beforeInteractive"
